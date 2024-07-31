fb7d5b83c45d0ef037c4189f52b14d60b55cbcf8
Hrishikesh-Panigrahi <hrishikesh.panigrahi38@gmail.com>
2024-07-31 20:58:05 +0530

    added frontend

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from django.views.decorators.csrf import csrf_exempt
-
 from . import views
 
 urlpatterns = [
     path("", csrf_exempt(views.index), name="index"),
+    path("about/", views.about, name="about"),
+    path("services/", views.services, name="services"),
+    path("contact/", views.contact, name="contact"),
 ]
--- base/views.py
+++ base/views.py
@@ -70,3 +70,12 @@ def index(request):
             return render(request, "index.html", context)
     else:
         return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def services(request):
+    return render(request, "services.html")
+
+def contact(request):
+    return render(request, "contact.html")
